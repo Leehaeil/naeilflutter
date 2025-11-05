2b034461765a8599b083c9aaf5ee3a65b6097731
haeilE <dlgodlf11@naeileun.dev>
2025-11-05 10:46:41 +0900

    chore: sample 프로젝트 폴더 제거
    - 생성된 sample 프로젝트를 Git에서 제거
    - .gitignore에 sample/ 디렉토리 무시 규칙 추가

--- sample/android/app/src/main/kotlin/com/example/fwefffs/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fwefffs
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- sample/android/app/src/main/kotlin/com/naeil/eun/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.naeil.eun
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
-
